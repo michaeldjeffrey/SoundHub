f1cb1e659fa03ed3c4d615b2e1bc033228979fbc
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-05-16 00:42:17 -0700

    change icon for play pause dragging functionality

--- js/AudioPlayer.js
+++ js/AudioPlayer.js
@@ -6,11 +6,11 @@ function togglePlayPause(){
   var playpause = document.getElementById('playpause');
   if(audio.paused || audio.ended){
     playpause.title = 'pause';
-    playpause.innerHTML = 'pause';
+    playpause.className = 'icon-pause';
     audio.play();
   } else {
     playpause.title = 'play';
-    playpause.innerHTML = 'play';
+    playpause.className = 'icon-play';
     audio.pause();
   }
 }
@@ -30,3 +30,48 @@ function changeSong(){
   console.log('changed called')
   SoundHub.PlaylistApp.nextSong();
 }
+
+
+
+
+
+$( function() {
+  $('#drag').each(function(){
+    var $drag = $(this);
+
+    $drag.on('mousedown', function(ev) {
+      audio.removeEventListener('timeupdate', updateProgress, false);
+      var $this = $( this );
+      var $parent = $this.parent();
+      var poffs = $parent.position();
+      var pwidth = $parent.width();
+      var ppwidth = $parent.parent().width()
+
+      var x = ev.pageX;
+      var y = ev.pageY;
+
+      $this.parent();
+
+      $(document).on('mousemove.dragging', function(ev){
+        var mx = ev.pageX;
+        var my = ev.pageY;
+
+        var rx = mx - x;
+        var ry = my - y;
+
+        var value = Math.floor((100 / ppwidth) * (pwidth + rx));
+
+        $parent.css({
+          'width' : (pwidth + rx) + 'px'
+        });
+        // console.log(value);
+        audio.currentTime = (value / 100) * audio.duration;
+      }).on('mouseup.dragging mouseleave.dragging', function(ev){
+        audio.addEventListener('timeupdate', updateProgress, false);
+        $(document).off('.dragging');
+      });
+    });
+  });
+});
+
+
