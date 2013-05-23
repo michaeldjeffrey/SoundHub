ccd070e733c4d78c866ad78fb28c8c02149a8840
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-05-22 19:03:54 -0700

    Make SoundCloud functions into Object

--- js/SCapi.js
+++ js/SCapi.js
@@ -2,24 +2,37 @@ SC.initialize({
 	client_id: "d945fc936728c08f4e2b720cc5998fd9"
 });
 
-function searchByGenre(genre, thisItem) {
-	SC.get('/tracks?limit=10', { genres: genre},
-	    function(tracks) {
-	    	SoundHub.ResultsApp.initializeLayout(tracks, genre);
-	    	// fillResults(tracks, genre)
-	    }
-	);
-}
+SoundHub.SoundCloudAPI = function(){
 
-// SC.stream("/tracks/293", function(sound){
-// 	console.log('sound loaded', sound)
-// 	// $("audio").append("<source src="+sound.url+" type='audio/mpeg'>")
-// });
+	var SoundCloudAPI = {};
 
-function playSong(songId){
-	SC.stream('/tracks/'+songId, function(sound){
-		console.log('playing: ' , sound);
-		$("audio").attr('src',sound.url);
-		$("audio").attr('autoplay','autoplay');
-	});
-}
+	var currentGenre;
+
+	SoundCloudAPI.searchByGenre = function(genre, thisItem){
+		currentGenre = genre;
+		SC.get('/tracks?limit=15', {genres: genre},
+			function(tracks){
+				SoundHub.ResultsApp.initializeLayout(tracks, genre);
+			}
+		);
+	}
+	SoundCloudAPI.moreSongs = function(page_size, page_offset){
+		SC.get('/tracks', {
+			genres: currentGenre, 
+			limit: page_size, 
+			offset: page_offset 
+		}, 
+			function(tracks) {
+	  		SoundHub.ResultsApp.addSongs(tracks)
+		});
+	}
+	SoundCloudAPI.playSong = function(songId){
+		SC.stream('/tracks/'+songId, function(sound){
+			$("audio").attr('src', sound.url);
+			SoundHub.AudioPlayer.togglePlayPause();
+		});
+	}
+
+	return SoundCloudAPI;
+
+}();
