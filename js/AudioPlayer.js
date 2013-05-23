f7872302fc95b7423c0cd17ee9f7b8e48586741c
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-05-22 18:58:16 -0700

    Turned Audioplayer into Object Bound all click events

--- js/AudioPlayer.js
+++ js/AudioPlayer.js
@@ -1,8 +1,32 @@
+//TODO: click on seek timeline, without having to grab scrub
+
+SoundHub.AudioPlayer = function(){
+
+var AudioPlayer = {};
 var audio = document.getElementById('audio-test');
 audio.controls = false;
-audio.addEventListener('timeupdate', updateProgress, false);
-audio.addEventListener('ended', changeSong, false)
-function togglePlayPause(){
+
+$(audio).on('timeupdate', function(){
+  updateProgress();
+});
+$(audio).on('ended', function(){
+  SoundHub.PlaylistApp.nextSong();
+})
+
+$("#playpause").on('click', function(){
+  AudioPlayer.togglePlayPause();
+});
+$(".icon-step-backward").on('click', function(){
+  AudioPlayer.previousSong();
+});
+$(".icon-step-forward").on('click', function(){
+  SoundHub.PlaylistApp.nextSong();
+})
+$("#volume").on('change', function(){
+  AudioPlayer.setVolume();
+})
+
+AudioPlayer.togglePlayPause = function(){
   var playpause = document.getElementById('playpause');
   if(audio.paused || audio.ended){
     playpause.title = 'pause';
@@ -14,64 +38,65 @@ function togglePlayPause(){
     audio.pause();
   }
 }
-function setVolume(){
+
+AudioPlayer.setVolume = function(){
   var volume = document.getElementById('volume');
   audio.volume = volume.value;
 }
+
 function updateProgress(){
   var progress = document.getElementById('progress');
   var value = 0;
   if(audio.currentTime > 0){
     value = Math.floor((100 / audio.duration) * audio.currentTime);
+    // value = (100 / audio.duration) * audio.currentTime;
   }
   progress.style.width = value + "%";
 }
-function changeSong(){
-  console.log('changed called')
-  SoundHub.PlaylistApp.nextSong();
-}
 
 
 
+$('#drag').each(function(){
+  var $drag = $(this);
 
+  $drag.on('mousedown', function(ev) {
+    audio.removeEventListener('timeupdate', updateProgress, false);
+    var $this = $( this );
+    var $parent = $this.parent();
+    var poffs = $parent.position();
+    var pwidth = $parent.width();
+    var ppwidth = $parent.parent().width()
 
-$( function() {
-  $('#drag').each(function(){
-    var $drag = $(this);
+    var x = ev.pageX;
+    var y = ev.pageY;
 
-    $drag.on('mousedown', function(ev) {
-      audio.removeEventListener('timeupdate', updateProgress, false);
-      var $this = $( this );
-      var $parent = $this.parent();
-      var poffs = $parent.position();
-      var pwidth = $parent.width();
-      var ppwidth = $parent.parent().width()
+    $this.parent();
 
-      var x = ev.pageX;
-      var y = ev.pageY;
+    $(document).on('mousemove.dragging', function(ev){
+      var mx = ev.pageX;
+      var my = ev.pageY;
 
-      $this.parent();
+      var rx = mx - x;
+      var ry = my - y;
 
-      $(document).on('mousemove.dragging', function(ev){
-        var mx = ev.pageX;
-        var my = ev.pageY;
+      var value = Math.floor((100 / ppwidth) * (pwidth + rx));
 
-        var rx = mx - x;
-        var ry = my - y;
-
-        var value = Math.floor((100 / ppwidth) * (pwidth + rx));
-
-        $parent.css({
-          'width' : (pwidth + rx) + 'px'
-        });
-        // console.log(value);
-        audio.currentTime = (value / 100) * audio.duration;
-      }).on('mouseup.dragging mouseleave.dragging', function(ev){
-        audio.addEventListener('timeupdate', updateProgress, false);
-        $(document).off('.dragging');
+      $parent.css({
+        'width' : (pwidth + rx) + 'px'
       });
+      // console.log(value);
+      audio.currentTime = (value / 100) * audio.duration;
+    }).on('mouseup.dragging mouseleave.dragging', function(ev){
+      audio.addEventListener('timeupdate', updateProgress, false);
+      $(document).off('.dragging');
     });
   });
 });
 
 
+
+
+
+return AudioPlayer;
+
+}();
